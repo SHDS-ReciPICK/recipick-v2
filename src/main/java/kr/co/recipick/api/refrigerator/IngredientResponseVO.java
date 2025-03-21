fa8333b111a512f0629ae2ab218aa348a4911653
eunjiko <e.melies1o14@gmail.com>
2025-03-22 02:15:33 +0900

    feat: 냉장고 기능을 연동을 위한 식재료 전달 요청 #19

---
+++ src/main/java/kr/co/recipick/api/refrigerator/IngredientResponseVO.java
@@ -0,0 +1,16 @@
+package kr.co.recipick.api.refrigerator;
+
+import lombok.Data;
+
+@Data
+public class IngredientResponseVO {
+    private int userId;
+    private String name;
+    private String emoji;
+    private String image;
+    private int quantity;
+    private String expiryDate;
+    private String mainCategory;
+    private String subCategory;
+    private String detailCategory;
+}
